14b9478af89f6910d2a27807c1213a077a944347
Jonny Mortemore <jonathanmortemore@gmail.com>
2025-11-07 17:45:42 +1100

    update: initial dropdown setup

---
+++ src/dropdown.js
@@ -0,0 +1,61 @@
+export class Dropdown {
+    constructor(
+        selectedValue,
+        contents,
+        selectedEl = document.querySelector(".dropdown-selected"),
+        containerEl = document.querySelector(".dropdown-container"),
+        dropdownEl = document.querySelector(".dropdown"),
+        buttonEl = document.querySelector(".show-dropdown-button"),
+    ) {
+        this.container = containerEl;
+
+        this.selectedValue = selectedValue;
+        this.selectedDiv = selectedEl;
+
+        this.selectedDiv.innerText = this.selectedValue;
+
+        this.dropdown = dropdownEl;
+        this.dropdown.style.display = "none";
+        this.dropdown.hidden = true;
+        this.dropdown.innerHTML = "";
+
+        this.button = buttonEl;
+
+        this.populateDropdown(contents);
+        this.assignButtonEventListener();
+    }
+
+    populateDropdown(contents) {
+        contents.forEach((content) => {
+            this.createDropdownElement(content);
+        });
+    }
+
+    createDropdownElement(content) {
+        const dropdownEl = document.createElement("div");
+        dropdownEl.className = "dropdown-element";
+        dropdownEl.innerText = content;
+        this.dropdown.appendChild(dropdownEl);
+    }
+
+    assignButtonEventListener() {
+        this.button.addEventListener("click", () => {
+            console.log("click");
+            if (this.dropdown.hidden === true) {
+                this.showDropdown();
+            } else {
+                this.hideDropdown();
+            }
+        });
+    }
+
+    hideDropdown() {
+        this.dropdown.hidden = true;
+        this.dropdown.style.display = "none";
+    }
+
+    showDropdown() {
+        this.dropdown.hidden = false;
+        this.dropdown.style.display = "flex";
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import "./styles.css" // all css files need to be imported into js
+import { Dropdown } from "./dropdown"
 
 //To import images using JS, import as any file: import odinImage from "./odin.png";
+
+console.log(new Dropdown(
+    "selectedValue",
+    [
+        "dropdown-1",
+        "dropdown-2"
+    ]
+));
