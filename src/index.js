e6ef3f0eaa1fb7e10ff95921a9dc88c3fb08b0c6
Jonny Mortemore <jonathanmortemore@gmail.com>
2025-11-10 19:08:58 +1100

    update: move dropdown css to seperate file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./styles.css" // all css files need to be imported into js
+import "./dropdown.css"
 import { Dropdown } from "./dropdown"
 
 //To import images using JS, import as any file: import odinImage from "./odin.png";
