aa424713ba8a0b168d2ec646c720d04ab845effd
Jonny Mortemore <jonathanmortemore@gmail.com>
2025-11-12 13:19:54 +1100

    update: simplified dropdown

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,42 +1,18 @@
 export class Dropdown {
     constructor(
-        selectedValue,
-        contents,
         selectedEl = document.querySelector(".dropdown-selected"),
         containerEl = document.querySelector(".dropdown-container"),
         dropdownEl = document.querySelector(".dropdown"),
         buttonEl = document.querySelector(".show-dropdown-button"),
     ) {
         this.container = containerEl;
-
-        this.selectedValue = selectedValue;
         this.selectedDiv = selectedEl;
-
-        this.selectedDiv.innerText = this.selectedValue;
-
         this.dropdown = dropdownEl;
         this.dropdown.style.display = "none";
         this.dropdown.hidden = true;
-        this.dropdown.innerHTML = "";
-
         this.button = buttonEl;
 
-        this.populateDropdown(contents);
         this.assignButtonEventListener();
-    }
-
-    populateDropdown(contents) {
-        contents.forEach((content) => {
-            this.createDropdownElement(content);
-        });
-    }
-
-    createDropdownElement(content) {
-        const dropdownEl = document.createElement("a");
-        dropdownEl.className = "dropdown-element";
-        dropdownEl.innerText = content.name;
-        dropdownEl.href = content.target;
-        this.dropdown.appendChild(dropdownEl);
     }
 
     assignButtonEventListener() {
--- src/index.js
+++ src/index.js
@@ -6,18 +6,6 @@ import { Carousel } from "./carousel";
 
 //To import images using JS, import as any file: import odinImage from "./odin.png";
 
-console.log(new Dropdown(
-    "selectedValue",
-    [
-        {
-            name: "dropdown-1",
-            target: "#target1"
-        },
-        {
-            name: "dropdown-2",
-            target: "#target2"
-        },
-    ]
-));
+console.log(new Dropdown());
 
 console.log(new Carousel());
